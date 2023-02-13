ac59b42d43c55ad76a1491d7e8b7c2714c56f5f6
Josue Hernandez <josuehdz0@gmail.com>
2023-02-13 16:13:43 -0700

    Get works on fakeDb

--- server/controllers/BandsController.js
+++ server/controllers/BandsController.js
@@ -1,8 +1,32 @@
+import { bandsService } from "../services/BandsService.js";
 import BaseController from "../utils/BaseController.js";
+import { logger } from "../utils/Logger.js";
 
 export class BandsController extends BaseController{
   constructor(){
     super('api/bands')
+
+    this.router
+      .get('', this.getBands)
+      .get('', this.getBandbyId)
+  }
+
+  getBands(req, res, next){
+    try {
+      let bands = bandsService.getBands()
+      res.send(bands)
+    } catch (error) {
+      next(error)
+    }
+  }
+  getBandbyId(res,req,next){
+    try {
+      let bandId = req.params.bandId
+      logger.log(bandId)
+      const band = 
+    } catch (error) {
+      next(error)
+    }
   }
 
 
